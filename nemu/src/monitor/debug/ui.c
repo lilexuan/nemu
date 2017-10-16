04df71b0c6e1ccba2d6b1d796286d3237efa75a1
kelvin <834524281@qq.com>
2017-10-16 12:44:24 +0800

    fuck

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,7 +46,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+//fuck
 	/* TODO: Add more commands */
 
 };
